113783f43a753614d06554ef52571c2dd35f4529
Vinicius Lourenco <vinicius_lourenco@outlook.com>
2024-05-31 13:19:50 -0300

    Implementation TASK1

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -13,6 +13,32 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
-     
- 
+
+    public static boolean isPalindrome(String str) {
+        String cleanedStr = str.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        int length = cleanedStr.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            if (cleanedStr.charAt(i) != cleanedStr.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string to check if it is a palindrome: ");
+        String input = sc.nextLine();
+
+        System.out.println("--------------------------------------------");
+        if (isPalindrome(input)) {
+            System.out.println("The string \"" + input + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is not a palindrome.");
+        }
+
+        sc.close();
+    }
 }
